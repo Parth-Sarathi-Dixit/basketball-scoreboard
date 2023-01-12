b99f33732fd617dce9bb812820e9bada063760fd
pARTh sARATHi dIXIt <110755500+Parth-Sarathi-Dixit@users.noreply.github.com>
2023-01-12 09:40:23 +0530

    Add files via upload

---
+++ Basketball Scorecard/index.js
@@ -0,0 +1,44 @@
+let score1 = document.querySelector("#score-1");
+let score2 = document.querySelector("#score-2");
+
+function clearScore(){
+    score1.innerHTML = 0;
+    score2.innerHTML = 0;
+}
+
+
+function increaseOne1(){
+    score1.innerHTML ++;
+}
+
+
+function increaseTwo1(){
+    score1.innerHTML ++;
+    score1.innerHTML ++;
+}
+
+
+function increaseThree1(){
+    score1.innerHTML ++;
+    score1.innerHTML ++;
+    score1.innerHTML ++;
+}
+
+
+
+function increaseOne2(){
+    score2.innerHTML ++;
+}
+
+
+function increaseTwo2(){
+    score2.innerHTML ++;
+    score2.innerHTML ++;
+}
+
+
+function increaseThree2(){
+    score2.innerHTML ++;
+    score2.innerHTML ++;
+    score2.innerHTML ++;
+}
