60b953217d3540aa67fe2129779dbb87aac06388
pARTh sARATHi dIXIt <110755500+Parth-Sarathi-Dixit@users.noreply.github.com>
2023-01-12 19:01:24 +0530

    Update index.js

--- Basketball Scorecard/index.js
+++ Basketball Scorecard/index.js
@@ -1,44 +1,48 @@
 let score1 = document.querySelector("#score-1");
 let score2 = document.querySelector("#score-2");
+let count1 = 0;
+let count2 = 0;
 
 function clearScore(){
     score1.innerHTML = 0;
     score2.innerHTML = 0;
+    count1 = 0;
+    count2 = 0;
 }
 
 
 function increaseOne1(){
-    score1.innerHTML ++;
+    count1 += 1;
+    score1.textContent = count1;
 }
 
 
 function increaseTwo1(){
-    score1.innerHTML ++;
-    score1.innerHTML ++;
+    count1 += 2;
+    score1.textContent = count1;
 }
 
 
 function increaseThree1(){
-    score1.innerHTML ++;
-    score1.innerHTML ++;
-    score1.innerHTML ++;
+    count1 += 3;
+    score1.textContent = count1;
 }
 
 
 
 function increaseOne2(){
-    score2.innerHTML ++;
+    count2 += 1;
+    score2.textContent = count2;
 }
 
 
 function increaseTwo2(){
-    score2.innerHTML ++;
-    score2.innerHTML ++;
+    count2 += 2;
+    score2.textContent = count2;
 }
 
 
 function increaseThree2(){
-    score2.innerHTML ++;
-    score2.innerHTML ++;
-    score2.innerHTML ++;
+    count2 += 3;
+    score2.textContent = count2;
 }
